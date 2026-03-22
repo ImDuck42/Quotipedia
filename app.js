005d6b4c113da753b7734f2921f43ebbb90943ec
ImDuck42 <lennygorafski@gmail.com>
2026-03-22 16:48:26 +0100

    update cdn

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const DEFAULT_CONFIG = {
     owner: 'ImDuck42',
     repo: 'Quotipedia',
     publicToken: 'ghdb_enc_ICEwKjIqGzImPBtzdgoFcBQOcAN3GSsXARAhKg8PFDEGFz0Adw4nKj0xBzJ/PykXETAqICgFLxoKHTUnPhwqKn97AxYXLBcPNTgwCxAfDnR0HwkaFyYgLhIkIg8T',
-    useCDN: true,
+    useCDN: false,
 }
 
 function getConfig() {
